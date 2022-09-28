25d283b40ce83a8c3121e88ef87ea8a78f33bffc
Makariuz <samuel.eduardo23@hotmail.com>
2022-09-28 15:39:52 +0100

    new

--- src/pages/new/New.js
+++ src/pages/new/New.js
@@ -1,61 +1,53 @@
 import "./new.scss";
 import Navbar from "../../components/navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
-import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
+import DriveFolderUploadOutlinedIcon from "@mui/icons-material/DriveFolderUploadOutlined";
+import { useState } from "react";
 
+const New = ({ inputs, title }) => {
+  const [file, setFile] = useState("");
 
-const New = () => {
   return (
     <div className="new">
       <Sidebar />
       <div className="newContainer">
         <Navbar />
         <div className="top">
-          <h1 className="addUser">Add New User</h1>
+          <h1 className="addUser">{title}</h1>
         </div>
         <div className="bottom">
           <div className="left">
             <img
-              src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              src={
+                file
+                  ? URL.createObjectURL(file)
+                  : "https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg"
+              }
               alt=""
             />
           </div>
           <div className="right">
             <form>
               <div className="formInput">
-                <label htmlFor="file">Image: <DriveFolderUploadOutlinedIcon className="icon" /></label>
-                <input type="file" id="file" style={{display:'none'}} />
+                <label htmlFor="file">
+                  Image: <DriveFolderUploadOutlinedIcon className="icon" />
+                </label>
+                <input
+                  type="file"
+                  id="file"
+                  onChange={(e) => setFile(e.target.files[0])}
+                  style={{ display: "none" }}
+                />
               </div>
-              <div className="formInput">
-                <label>Username:</label>
-                <input type="text" placeholder="john_doe" />
-              </div>
-              <div className="formInput">
-                <label>Name and Surname:</label>
-                <input type="text" placeholder="John Doe" />
-              </div>
-              <div className="formInput">
-                <label>Email:</label>
-                <input type="email" placeholder="john_doe@gmail.com" />
-              </div>
-              <div className="formInput">
-                <label>Phone:</label>
-                <input type="text" placeholder="+123 999322 22" />
-              </div>
-              <div className="formInput">
-                <label>Address:</label>
-                <input type="text" placeholder="123 Street" />
-              </div>
-              <div className="formInput">
-                <label>Country:</label>
-                <input type="text" placeholder="USA" />
-              </div>
-              <div className="formInput">
-                <label>Password:</label>
-                <input type="pasword"  />
-              </div>
-              <button>Send</button>
 
+              {inputs.map((input) => (
+                <div className="formInput" key={input.id}>
+                  <label>{input.label}:</label>
+                  <input type={input.type} placeholder={input.placeholder} />
+                </div>
+              ))}
+
+              <button>Send</button>
             </form>
           </div>
         </div>

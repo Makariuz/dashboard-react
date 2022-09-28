8e9238dfeea5e485c751b86c7ff9aa39c2777d3d
Makariuz <samuel.eduardo23@hotmail.com>
2022-09-28 13:51:57 +0100

    featured

---
+++ src/components/featured/Featured.js
@@ -0,0 +1,42 @@
+import "./featured.scss";
+import MoreVertOutlinedIcon from "@mui/icons-material/MoreVertOutlined";
+import { CircularProgressbar } from "react-circular-progressbar";
+import 'react-circular-progressbar/dist/styles.css';
+import ArrowDropDownOutlinedIcon from '@mui/icons-material/ArrowDropDownOutlined';
+import ArrowDropUpOutlinedIcon from '@mui/icons-material/ArrowDropUpOutlined';
+
+const Featured = () => {
+  return (
+    <div className="featured">
+      <div className="top">
+        <h1 className="title">Total Revenue</h1>
+        <MoreVertOutlinedIcon fontSize="small" />
+      </div>
+      <div className="bottom">
+        <div className="featuredChart">
+          <CircularProgressbar value={70} text={"70%"} strokeWidth={5} />
+        </div>
+        <p className="title">Total sales made today</p>
+        <p className="amount">€ 420</p>
+        <p className="desc">Previous transactions processing. Last payments may not be included.</p>
+        <div className="summary">
+          <div className="item">
+          <div className="itemTitle">Target</div>
+          <div className="itemResult"> <ArrowDropDownOutlinedIcon fontSize="small" /> € 12.4k</div>
+          </div>
+          <div className="item">
+          <div className="itemTitle">Last Week</div>
+          <div className="itemResult positive"> <ArrowDropUpOutlinedIcon fontSize="small" /> € 12.4k</div>
+          </div>
+          <div className="item">
+          <div className="itemTitle">Last Month</div>
+          <div className="itemResult negative"> <ArrowDropDownOutlinedIcon fontSize="small" /> € 12.4k</div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Featured;
